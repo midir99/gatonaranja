fe10264089d71a3dc83b527a51e6eb715135282c
midir99 <midir99@proton.me>
2026-03-21 15:11:46 -0600

    guardar progreso

--- cmd.go
+++ cmd.go
@@ -14,26 +14,7 @@ type Config struct {
 	TelegramBotToken string
 }
 
-// ParseConfig parses command-line flags, falls back to environment variables
-// when needed, and returns the runtime configuration for the bot.
-func ParseConfig() (Config, error) {
-	// Parse the flags
-	var authorizedUsers string
-	var telegramBotToken string
-	flag.StringVar(
-		&authorizedUsers,
-		"authorized-users",
-		"",
-		"A comma-separated list of Telegram user IDs allowed to use the bot (defaults to AUTHORIZED_USERS). If no IDs are specified, everyone can use the bot",
-	)
-	flag.StringVar(
-		&telegramBotToken,
-		"telegram-bot-token",
-		"",
-		"The Telegram bot token used to authenticate this bot (defaults to TELEGRAM_BOT_TOKEN)",
-	)
-	flag.Parse()
-
+func validateAuthorizedUsers(authorizedUsers string) ([]int64, error) {
 	// Process the authorized-users value
 	authorizedUsers = strings.TrimSpace(authorizedUsers)
 	if authorizedUsers == "" {
@@ -46,25 +27,70 @@ func ParseConfig() (Config, error) {
 		for _, userID := range authorizedUsersArray {
 			userIDInt, err := strconv.ParseInt(strings.TrimSpace(userID), 10, 0)
 			if err != nil {
-				return Config{}, fmt.Errorf("invalid authorized user ID %q: must be a valid Telegram user ID", userID)
+				return nil, fmt.Errorf("invalid authorized user ID %q: must be a valid Telegram user ID", userID)
 			}
 			authorizedUsersIntArray = append(authorizedUsersIntArray, userIDInt)
 		}
 	}
+	return authorizedUsersIntArray, nil
+}
 
+func validateTelegramBotToken(telegramBotToken string) (string, error) {
 	// Process the telegram-bot-token value
 	telegramBotToken = strings.TrimSpace(telegramBotToken)
 	if telegramBotToken == "" {
 		telegramBotToken = os.Getenv("TELEGRAM_BOT_TOKEN")
 		telegramBotToken = strings.TrimSpace(telegramBotToken)
 		if telegramBotToken == "" {
-			return Config{}, fmt.Errorf("telegram bot token is required: use -telegram-bot-token or TELEGRAM_BOT_TOKEN")
+			return "", fmt.Errorf("telegram bot token is required: use -telegram-bot-token or TELEGRAM_BOT_TOKEN")
 		}
+	}
+	return telegramBotToken, nil
+}
+
+func fallbackToEnv(variable string, envVariableName string) string {
+
+}
+
+// ParseConfig parses command-line flags, falls back to environment variables
+// when needed, and returns the runtime configuration for the bot.
+func ParseConfig(args []string) (Config, error) {
+
+	// Use a FlagSet for easier unit testing
+	fs := flag.NewFlagSet("gatonaranja", flag.ContinueOnError)
+
+	// Parse the flags
+	var authorizedUsers string
+	var telegramBotToken string
+
+	fs.StringVar(
+		&authorizedUsers,
+		"authorized-users",
+		"",
+		"A comma-separated list of Telegram user IDs allowed to use the bot (defaults to AUTHORIZED_USERS). If no IDs are specified, everyone can use the bot",
+	)
+	fs.StringVar(
+		&telegramBotToken,
+		"telegram-bot-token",
+		"",
+		"The Telegram bot token used to authenticate this bot (defaults to TELEGRAM_BOT_TOKEN)",
+	)
+	if err := fs.Parse(args); err != nil {
+		return Config{}, err
+	}
+
+	authorizedUsersArray, err := validateAuthorizedUsers(authorizedUsers)
+	if err != nil {
+		return Config{}, err
+	}
+	telegramBotToken, err = validateTelegramBotToken(telegramBotToken)
+	if err != nil {
+		return Config{}, err
 	}
 
 	// Prepare the configuration
 	config := Config{
-		AuthorizedUsers:  authorizedUsersIntArray,
+		AuthorizedUsers:  authorizedUsersArray,
 		TelegramBotToken: telegramBotToken,
 	}
 	return config, nil
